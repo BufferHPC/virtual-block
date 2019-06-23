10f87b86847c348d09163a1b21482eb6d1a56d42
buhe <buhe@royole.com>
2019-06-23 13:03:53 +0800

    修正info页面高度问题

--- src/pages/info.vue
+++ src/pages/info.vue
@@ -45,7 +45,7 @@ export default {};
 .container {
   width: 100%;
   max-width: 800px;
-  height: 100%;
+  height: 150%;
   margin: 0 auto;
   box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
   .header {
