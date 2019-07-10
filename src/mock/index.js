aeb9d9b0ec794362e36c7acf92161b7b37de53ba
buhe <514983242@qq.com>
2019-07-10 21:51:50 +0800

    代码优化：优化mock代码相应的结果

--- src/mock/index.js
+++ src/mock/index.js
@@ -2,7 +2,7 @@
 const Mock = require('mockjs');
 const express = require('express');
 const app = express();
-
+//生成模拟的数据
 function generatorList(num) {
   return Mock.mock({
     [`list|${num}`]: [{
@@ -13,22 +13,20 @@ function generatorList(num) {
     }]
   });
 }
-
-app.all('/data', function(req, res, next) {
+//允许跨域请求返回数据
+app.all('/data', function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
-
+//截取路由并反馈数据
 app.get("/data", function (req, res) {
-  const {num} = req.query
+  const { num } = req.query;
   return res.send(generatorList(num));
 })
-
+//设置端口并打印对应调用结果
 const server = app.listen(4000, function () {
-  const host = "localhost"
-  const port = server.address().port
-  console.log("http://%s:%s/data?num=100", host, port)
+  console.log("http://%s:%s/data?num=100", server.address().port);
 })
